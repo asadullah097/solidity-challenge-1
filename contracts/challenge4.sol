39174c42f3d78d416d6d9156bb1024b7850ae8b6
asadullah097 <asaddir5566@gmail.com>
2022-07-02 12:24:07 +0500

    updated challenge 4 and 5

--- contracts/challenge4.sol
+++ contracts/challenge4.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 contract Payable {
     address payable public owner;
+    uint public _amount ;
 
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
+   
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+    function withdraw() public checkTime {
         uint256 amount = address(this).balance;
         (bool success, ) = owner.call{value: amount}("");
         require(success, "Failed to send Ether");
+    }
+     function sendEth(address payable _to) public payable {
+         _to.transfer(_amount);
+    }
+
+
+    function getBalance() public view returns (uint) {
+         return address(this).balance;
     }
 }
--- contracts/challenge5.sol
+++ contracts/challenge5.sol
@@ -3,35 +3,37 @@ pragma solidity ^0.8.0;
 
 contract Payable {
     address payable public owner;
-    uint256 public withdrawalTime;
+     uint256 unlockDate;
+     uint public _amount ;
 
     constructor() payable {
         owner = payable(msg.sender);
-        withdrawalTime = 1 days;
+        unlockDate=2 days;
+        _amount = 5 ether;
     }
 
     modifier checkTime() {
-        // require(block.timestamp= );
+        uint startDate = block.timestamp; // 2018-01-01 00:00:00
+        uint endDate = unlockDate; // 2018-02-10 00:00:00
+        uint diff = (endDate - startDate);
+        require(diff == 0);
         _;
     }
-
     receive() external payable {}
 
     // Fallback function is called when msg.data is not empty
     fallback() external payable {}
-
-    function withdraw() public {
+    function withdraw() public checkTime {
         uint256 amount = address(this).balance;
         (bool success, ) = owner.call{value: amount}("");
         require(success, "Failed to send Ether");
     }
-
-    function sendEth(address payable _to, uint256 _amount) public payable {
-        bool sent = _to.send(_amount);
-        require(sent, "Failed to send Ether");
+     function sendEth(address payable _to) public payable {
+         _to.transfer(_amount);
     }
 
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
+
+    function getBalance() public view returns (uint) {
+         return address(this).balance;
     }
 }
